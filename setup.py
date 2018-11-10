58103fa5201cdf0bfbc5e3b01e377b58a4361f0f
Coumes Quentin <qcoumes@etud.u-pem.fr>
2018-11-10 21:02:34 +0100

    travis deploy

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ CLASSIFIERS = [
     'License :: OSI Approved :: MIT License',
     'Natural Language :: English',
     'Operating System :: POSIX :: Linux',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
