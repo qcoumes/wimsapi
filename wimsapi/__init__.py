b9630874e5da798e4f6249997e4aadb1d407069a
Coumes Quentin <qcoumes@etud.u-pem.fr>
2018-11-29 14:03:40 +0100

    Format code

--- wimsapi/__init__.py
+++ wimsapi/__init__.py
@@ -3,7 +3,8 @@
 from .api import WimsAPI
 from .user import User
 from .wclass import Class
-from .exceptions import AdmRawError, NotSavedError, WimsAPIError
+from .exceptions import AdmRawError, NotSavedError, WimsAPIError, InvalidItemTypeError
+
 
 name = "wimsapi"
 __title__ = 'wimsapi'
