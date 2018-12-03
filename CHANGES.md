9463cbcbeaa6031520a11f138d35170acce7a85b
Coumes Quentin <qcoumes@etud.u-pem.fr>
2018-12-03 15:43:10 +0100

    fix setup.py

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.3.1
+
+* Fixed buggy import in setup.py
+
+
 ## 0.3.0
 
 * Adding **Classe** higher level API, allowing to manipulate a WIMS' Class.
--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 import codecs
 import os
 
-import wimsapi
-
 
 try:
     from setuptools import setup
@@ -32,7 +30,7 @@ long_description = (
 
 setup(
     name='wimsapi',
-    version=wimsapi.__version__,
+    version=0.3.1,
     description='A Python 3 implementation of WIMS adm/raw module.',
     long_description=long_description,
     long_description_content_type='text/markdown',
