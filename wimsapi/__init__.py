57a0b1a5fbcb04d2ad1cb61cdf8f160195a335a7
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-05-11 21:23:02 +0200

    Added Sheet to init

--- wimsapi/__init__.py
+++ wimsapi/__init__.py
@@ -2,6 +2,7 @@
 
 from .api import WimsAPI
 from .exceptions import AdmRawError, InvalidItemTypeError, NotSavedError, WimsAPIError
+from .sheet import Sheet
 from .user import User
 from .wclass import Class
 
